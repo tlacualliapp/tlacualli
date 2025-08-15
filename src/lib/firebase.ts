190cfcce130fec29721bf50ead7bc8654d36f1f9
Your Name <you@example.com>
2025-08-15 21:28:53 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  projectId: "tlacualli-bfbh0",
-  appId: "1:918335842280:web:9756be54035734c080265d",
-  storageBucket: "tlacualli-bfbh0.firebasestorage.app",
-  apiKey: "AIzaSyB627iXGidjNf_rXMn4Y7dH2Gfw4X78IcE",
-  authDomain: "tlacualli-bfbh0.firebaseapp.com",
-  messagingSenderId: "918335842280",
+  "projectId": "tlacualli-bfbh0",
+  "appId": "1:918335842280:web:9756be54035734c080265d",
+  "storageBucket": "tlacualli-bfbh0.firebasestorage.app",
+  "apiKey": "AIzaSyB627iXGidjNf_rXMn4Y7dH2Gfw4X78IcE",
+  "authDomain": "tlacualli-bfbh0.firebaseapp.com",
+  "messagingSenderId": "918335842280"
 };
 
 // Initialize Firebase
