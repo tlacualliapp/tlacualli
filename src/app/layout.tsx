ba52af442826ac10cf08a7674a78af1d34c55285
Your Name <you@example.com>
2025-08-15 18:40:00 +0000

    el tema por defecto debe ser light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
         >
