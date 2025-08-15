8f54a063f252efe0da06cf4e9ff3c0cdffcfedbc
tlacualli app <tlacualli.app@gmail.com>
2025-08-15 17:12:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/components/icons/tables.tsx
@@ -0,0 +1,22 @@
+import type { SVGProps } from 'react';
+
+export const TablesIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M2 3h20" />
+    <path d="M21 3v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V3" />
+    <path d="M21 16v5" />
+    <path d="M3 16v5" />
+    <path d="M12 3v13" />
+  </svg>
+);
