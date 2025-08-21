f0726aee30537dddf12fd2d32bfaf248fcb703bd
tlacualli app <tlacualli.app@gmail.com>
2025-08-21 16:02:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -104,7 +104,6 @@ const DialogDescription = React.forwardRef<
     ref={ref}
     className={cn("text-sm text-muted-foreground", className)}
     {...props}
-    asChild
   />
 ))
 DialogDescription.displayName = DialogPrimitive.Description.displayName
