3445112e9627e35d71374811810c0db9f9e86b28
tlacualli app <tlacualli.app@gmail.com>
2025-08-27 20:25:45 +0000

    no quiero usar hostinger sino gmail: NEXT_PUBLIC_EMAIL_HOST=smtp.gmail.c

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -20,7 +20,7 @@ const getTransporter = () => {
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT),
-      secure: true, // true for 465, false for other ports
+      secure: false, // true for 465, false for other ports like 587
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
