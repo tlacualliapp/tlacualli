62aec58cc14ee3eb1e574760993e4ac700843931
tlacualli app <tlacualli.app@gmail.com>
2025-08-27 20:28:22 +0000

    Failed to send email. Invalid login username and pasword not accepted

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -20,13 +20,13 @@ const getTransporter = () => {
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT),
-      secure: false, // true for 465, false for other ports like 587
+      secure: true, // Use true for port 465, false for other ports
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      connectionTimeout: 10000, // 10 seconds
-      socketTimeout: 10000, // 10 seconds
+      connectionTimeout: 10000,
+      socketTimeout: 10000,
     });
 }
 
