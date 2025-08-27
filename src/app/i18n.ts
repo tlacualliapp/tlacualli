df9b86851be4bd9c4d74a832938ad42b36332dfc
tlacualli app <tlacualli.app@gmail.com>
2025-08-27 18:22:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -1,9 +1,11 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
   .use(HttpApi)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     supportedLngs: ['en', 'es'],
@@ -15,6 +17,9 @@ i18n
      react: {
       useSuspense: false,
     },
+    detection: {
+      order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
+    }
   });
 
 export default i18n;
