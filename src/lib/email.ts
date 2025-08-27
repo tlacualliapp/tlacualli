5c52dadd9eaedc8f5f48fe83bdb889d7819c6402
tlacualli app <tlacualli.app@gmail.com>
2025-08-27 20:24:22 +0000

    ahora me da: failed to sen email. connect ECONNREFUSED

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -20,11 +20,13 @@ const getTransporter = () => {
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT),
-      secure: true, // Use true for port 465, false for others
+      secure: true, // true for 465, false for other ports
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+      connectionTimeout: 10000, // 10 seconds
+      socketTimeout: 10000, // 10 seconds
     });
 }
 
